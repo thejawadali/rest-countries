7a01a023aa8d22e91cfc874c412086091eeb16a2
Jawad Ali <alijamil734@gmail.com>
2021-08-09 22:47:52 +0500

    made country component

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,18 @@
 <template>
-  <h1>Countries</h1>
+  <div class="w-full h-screen bg-gray-50 flex justify-center items-center">
+
+  </div>
 </template>
 
 <script lang="ts" setup>
+import Country from "./components/Country.vue";
+// const country= {
+//   name: "Pakistan",
+//   capital: "Islamabad",
+//   population: "123,123123.09",
+//   region: "Asia",
+//   neighbours: ["China", "Iran", "Afghanistan", "India"]
+// }
 </script>
 
 <style>
---
+++ src/components/Country.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="overflow-hidden shadow-lg rounded-md h-90 w-80 m-auto">
+    <img
+      alt="flag photo"
+      :src="country.img"
+      class="max-h-40 w-full object-cover border-b"
+    />
+    <div class="bg-white w-full p-4">
+      <p class="text-gray-800 text-xl font-bold mb-2">{{ country.name }}</p>
+      <p class="text-xs font-semibold">
+        Population: <span class="font-normal text-gray-500">{{country.population}}</span>
+      </p>
+      <p class="text-xs font-semibold">
+        Region: <span class="font-normal text-gray-500">{{country.region}}</span>
+      </p>
+      <p class="text-xs font-semibold">
+        Capital: <span class="font-normal text-gray-500">{{country.capital}}</span>
+      </p>
+      <p class="text-xs font-semibold">
+        Neighbours: <span v-for="i in country.neighbours" :key="i" class="font-normal text-gray-500">{{i}}, </span>
+      </p>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { toRefs } from 'vue'
+
+
+export default{
+  props: ["country"],
+  setup(props: Readonly<{ country: any}>) {
+    const {country} = toRefs(props)
+  }
+}
+</script>
