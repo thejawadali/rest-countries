5cf57a36eee1b60ff5c1850d17b432d2be02608e
Jawad Ali <alijamil734@gmail.com>
2021-08-10 22:45:09 +0500

    fetched data from api and displayed it

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,55 @@
 <template>
-  <div class="w-full h-screen bg-gray-50 flex justify-center items-center">
-
+  <div class="w-full h-screen bg-gray-50">
+    <nav
+      class="
+        bg-gray-200
+        py-3
+        px-8
+        shadow-lg shadow-gray-500
+        flex
+        justify-between
+      "
+    >
+      <h1 class="inline text-lg font-bold">Countries Data</h1>
+    </nav>
+    <main
+      v-if="countriesData.length > 0"
+      class="w-full p-10 flex flex-wrap gap-5 justify-center lg:justify-start"
+    >
+      <Country
+        v-for="country in countriesData"
+        :key="country.name"
+        :country="country"
+      ></Country>
+    </main>
   </div>
 </template>
 
 <script lang="ts" setup>
+import { computed, onMounted, ref } from "@vue/runtime-core";
 import Country from "./components/Country.vue";
-// const country= {
-//   name: "Pakistan",
-//   capital: "Islamabad",
-//   population: "123,123123.09",
-//   region: "Asia",
-//   neighbours: ["China", "Iran", "Afghanistan", "India"]
-// }
+const countriesData = ref({} as any);
+const search = ref("");
+
+function Search() {
+  fetch(`https://restcountries.eu/rest/v2/name/${search.value}`)
+    .then((res) => res.json())
+    .then((res) => {
+      console.log("data loaded");
+      countriesData.value = res;
+    });
+}
+
+
+onMounted(() => {
+  // fetch("https://restcountries.eu/rest/v2/region/europe")
+  fetch("https://restcountries.eu/rest/v2/all")
+    .then((res) => res.json())
+    .then((res) => {
+      console.log("data loaded");
+      countriesData.value = res;
+    });
+});
 </script>
 
 <style>
--- src/components/Country.vue
+++ src/components/Country.vue
@@ -1,24 +1,12 @@
 <template>
-  <div class="overflow-hidden shadow-lg rounded-md h-90 w-80 m-auto">
+  <div class="overflow-hidden shadow-lg rounded-md h-90 w-80 cursor-pointer m-auto bg-white active:bg-gray-100">
     <img
       alt="flag photo"
-      :src="country.img"
+      :src="country.flag"
       class="max-h-40 w-full object-cover border-b"
     />
-    <div class="bg-white w-full p-4">
-      <p class="text-gray-800 text-xl font-bold mb-2">{{ country.name }}</p>
-      <p class="text-xs font-semibold">
-        Population: <span class="font-normal text-gray-500">{{country.population}}</span>
-      </p>
-      <p class="text-xs font-semibold">
-        Region: <span class="font-normal text-gray-500">{{country.region}}</span>
-      </p>
-      <p class="text-xs font-semibold">
-        Capital: <span class="font-normal text-gray-500">{{country.capital}}</span>
-      </p>
-      <p class="text-xs font-semibold">
-        Neighbours: <span v-for="i in country.neighbours" :key="i" class="font-normal text-gray-500">{{i}}, </span>
-      </p>
+    <div class="bg-transparent w-full h-20 p-4 flex items-center">
+      <p class="text-gray-800 text-lg font-bold mb-2">{{ country.name }}</p>
     </div>
   </div>
 </template>
