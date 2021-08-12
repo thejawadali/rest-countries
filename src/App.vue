5da7062b4f814a6797beafe918fd19518128b1bc
Jawad Ali <alijamil734@gmail.com>
2021-08-12 19:40:57 +0500

    applied filters

--- src/App.vue
+++ src/App.vue
@@ -32,13 +32,12 @@
       >
         <input
           v-model="search"
-          @keypress.enter="searchByName"
           type="text"
           class="border-none py-2 pl-4 bg-transparent outline-none"
           placeholder="Search Country"
         />
         <span
-          @click="clearSearch"
+          @click="search = ''"
           v-if="search"
           class="material-icons cursor-pointer mr-1"
           >close</span
@@ -46,23 +45,31 @@
       </div>
     </nav>
     <!-- Nav Ends here -->
+
     <div class="absolute top-14 left-0 right-0">
       <!-- Sidenav starts here -->
-      <aside class="h-screen fixed bg-red-200 w-40 left-0">side nav</aside>
+      <aside class="h-screen fixed bg-gray-200 w-40 left-0 px-2">
+        <div class="mt-4">
+          <span class="text-gray-700">Select Region</span>
+          <div class="mt-2 flex flex-col">
+            <label
+              v-for="r in regions"
+              :key="r"
+              class="inline-flex items-center"
+            >
+              <input type="radio" v-model="region" :value="r" />
+              <span class="ml-2">{{ r }}</span>
+            </label>
+          </div>
+        </div>
+      </aside>
       <!-- Side Nav ends here -->
 
       <!-- Main Starts here -->
       <div class="absolute left-40">
         <main
           v-if="countriesData.length > 0"
-          class="
-            w-full
-            p-10
-            flex flex-wrap
-            gap-5
-            justify-center
-            lg:justify-start
-          "
+          class="p-10 flex flex-wrap gap-5 justify-center lg:justify-start"
         >
           <Country
             v-for="country in countriesData"
@@ -77,64 +84,60 @@
 </template>
 
 <script lang="ts" setup>
-import { computed, onMounted, ref } from "vue";
+import { onMounted, ref, watch } from "vue";
 import Country from "./components/Country.vue";
 import axios from "axios";
+import { lowerCase } from "lodash";
 
 const regions = [
+  "All",
   "Asia",
   "Europe",
   "Oceania",
   "Africa",
   "Americas",
   "Polar",
-  "Northern Africa",
 ];
+
 const countriesData = ref({} as any);
-const search = ref("uk");
-const region = ref("");
+const fetchedData = ref({} as any);
+const search = ref("");
+const region = ref();
 
-function clearSearch() {
-  search.value = "";
-  fetchAllData();
+// watch search value and load data accordingly
+watch(search, searchByName);
+
+// watcher on regions
+watch(region, searchByRegion);
+
+function searchByName() {
+  // filter data and show
+  countriesData.value = fetchedData.value;
+  countriesData.value = countriesData.value.filter((d: any) =>
+    lowerCase(d.name).includes(lowerCase(search.value))
+  );
 }
 
-async function searchByName() {
-  try {
-    const { data } = await axios.get(
-      `https://restcountries.eu/rest/v2/name/${search.value}`
-    );
-    countriesData.value = data;
-  } catch (error) {
-    // Show Error Msg
-    console.error("Error occured " + error);
+function searchByRegion() {
+  countriesData.value = fetchedData.value;
+  if (region.value === "All") {
+    countriesData.value = fetchedData.value;
+    return;
   }
+  countriesData.value = countriesData.value.filter(
+    (d: any) => lowerCase(d.region) == lowerCase(region.value)
+  );
 }
 
-async function searchByRegion() {
-  try {
-    const { data } = await axios.get(
-      `https://restcountries.eu/rest/v2/region/${region.value}`
-    );
-    countriesData.value = data;
-  } catch (error) {
-    console.error(error);
-  }
-}
-
-async function fetchAllData() {
+onMounted(async () => {
   try {
     const { data } = await axios.get("https://restcountries.eu/rest/v2/all");
+    fetchedData.value = data;
     countriesData.value = data;
   } catch (error) {
     // Show Error Msg
     console.error(error);
   }
-}
-
-onMounted(() => {
-  fetchAllData();
-  // searchByName();
 });
 </script>
 
--- src/components/Country.vue
+++ src/components/Country.vue
@@ -1,24 +1,59 @@
 <template>
-  <div class="overflow-hidden shadow-lg rounded-md h-90 w-80 cursor-pointer m-auto bg-white active:bg-gray-100">
+  <div
+    class="
+      overflow-hidden
+      shadow-lg
+      rounded-md
+      h-90
+      w-80
+      cursor-pointer
+      m-auto
+      bg-white
+      active:bg-gray-100
+    "
+  >
     <img
       alt="flag photo"
       :src="country.flag"
       class="max-h-40 w-full object-cover border-b"
     />
-    <div class="bg-transparent w-full h-20 p-4 flex items-center">
+    <div class="bg-transparent w-full h-36 p-4">
       <p class="text-gray-800 text-lg font-bold mb-2">{{ country.name }}</p>
+      <p class="text-xs font-semibold">
+        Population:
+        <span class="font-normal text-gray-500">{{ country.population }}</span>
+      </p>
+      <p class="text-xs font-semibold">
+        Region:
+        <span class="font-normal text-gray-500">{{ country.region }}</span>
+      </p>
+      <p class="text-xs font-semibold">
+        Capital:
+        <span class="font-normal text-gray-500">{{ country.capital }}</span>
+      </p>
+      <p v-if="country.borders.length > 0" class="text-xs font-semibold">
+        Neighbours:
+        <span
+          v-for="i in country.borders"
+          :key="i"
+          class="font-normal text-gray-500"
+          >{{ i }},
+        </span>
+      </p>
+      <p v-else class="text-xs font-semibold">
+        No Neighbour
+      </p>
     </div>
   </div>
 </template>
 
 <script lang="ts">
-import { toRefs } from 'vue'
+import { toRefs } from "vue";
 
-
-export default{
+export default {
   props: ["country"],
-  setup(props: Readonly<{ country: any}>) {
-    const {country} = toRefs(props)
-  }
-}
+  setup(props: Readonly<{ country: any }>) {
+    const { country } = toRefs(props);
+  },
+};
 </script>
